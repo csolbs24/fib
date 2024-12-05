b2772efbe180ec3d857a68fd5c0d67038f0e7b25
Carter Solberg <csolbs24@gmail.com>
2024-12-05 16:18:11 -0700

    Least viable product

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+console.log("hello brendans");
+const button_element = document.querySelector("button");
+console.log(button_element);
+button_element === null || button_element === void 0 ? void 0 : button_element.addEventListener("click", () => {
+    console.log("Hello Brendans with a capital b");
+    console.log(fib(10));
+    // Grab the input and output elements
+    const input_element = document.querySelector("input");
+    const output_element = document.querySelector("#output");
+    // If either one is null for whatever reason then exit the function
+    if (input_element === null || output_element === null)
+        return;
+    // Get what n to calculate up to and convert it to a number
+    const max_n_string = input_element.value;
+    const max_n = parseInt(max_n_string);
+    for (let i = 1; i <= max_n; i++) {
+        const current_output_element = document.createElement("div");
+        current_output_element.innerText = `${i}: ${fib(i)}`;
+        output_element.appendChild(current_output_element);
+    }
+});
+const fib = (current_number, memowise = {}) => {
+    if (current_number == 1)
+        return 1;
+    if (current_number == 2)
+        return 1;
+    if (current_number in memowise) {
+        return memowise[current_number];
+    }
+    return fib(current_number - 1) + fib(current_number - 2);
+};
---
+++ index.ts
@@ -0,0 +1,36 @@
+console.log("hello brendans")
+
+const button_element: HTMLButtonElement | null = document.querySelector("button")
+console.log(button_element)
+
+button_element?.addEventListener("click", () => {
+    console.log("Hello Brendans with a capital b")
+    console.log(fib(10))
+    
+    // Grab the input and output elements
+    const input_element: HTMLInputElement | null = document.querySelector("input")
+    const output_element: HTMLDivElement | null = document.querySelector("#output")
+
+    // If either one is null for whatever reason then exit the function
+    if (input_element === null || output_element === null) return
+
+    // Get what n to calculate up to and convert it to a number
+    const max_n_string: string | undefined = input_element.value
+    const max_n: number = parseInt(max_n_string)
+    
+    for (let i = 1; i <= max_n; i++) {
+        const current_output_element = document.createElement("div")
+        current_output_element.innerText = `${i}: ${fib(i)}`
+        output_element.appendChild(current_output_element)
+    }
+})
+
+const fib = (current_number: number, memowise: {[key: number]: number} = {}): number => {
+    if (current_number == 1) return 1
+    if (current_number == 2) return 1
+    if (current_number in memowise) {
+        return memowise[current_number]
+    }
+    
+    return fib(current_number - 1) + fib(current_number - 2)
+}
