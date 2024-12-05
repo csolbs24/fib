185e22c21c975b2866958d0ad30fd985cf5ecbe3
Carter Solberg <csolbs24@gmail.com>
2024-12-05 16:59:21 -0700

    Be evil

--- index.js
+++ index.js
@@ -12,23 +12,34 @@ button_element === null || button_element === void 0 ? void 0 : button_element.a
     // Get what n to calculate up to and convert it to a number
     const max_n_string = input_element.value;
     const max_n = parseInt(max_n_string);
+    // Create an array to hold the generated fib numbers
     let fib_numbers = [];
     for (let i = 1; i <= max_n; i++) {
         fib_numbers.push(fib(i));
     }
-    for (const [i, fib_number] of fib_numbers.entries()) {
+    // Type the enumeration of the fib entries as a tuple of two numbers
+    let enumeration;
+    for (enumeration of fib_numbers.entries()) {
         const current_output_element = document.createElement("div");
-        current_output_element.innerText = `${i}: ${fib_number}`;
+        current_output_element.innerText = `${enumeration[0]}: ${enumeration[1]}`;
         output_element.appendChild(current_output_element);
     }
 });
 const fib = (current_number, memowise = {}) => {
-    if (current_number == 1)
-        return 1;
-    if (current_number == 2)
-        return 1;
+    // Check if we've already memoised the answer
     if (current_number in memowise) {
         return memowise[current_number];
     }
-    return fib(current_number - 1) + fib(current_number - 2);
+    // The first two fib numbers are 1
+    if (current_number == 1) {
+        memowise[current_number] = 1;
+        return 1;
+    }
+    if (current_number == 2) {
+        memowise[current_number] = 1;
+        return 1;
+    }
+    const current_fib = fib(current_number - 1) + fib(current_number - 2);
+    memowise[current_number] = current_fib;
+    return current_fib;
 };
--- index.ts
+++ index.ts
@@ -15,23 +15,37 @@ button_element?.addEventListener("click", () => {
     const max_n_string: string | undefined = input_element.value
     const max_n: number = parseInt(max_n_string)
     
+    // Create an array to hold the generated fib numbers
     let fib_numbers: number[] = []
     for (let i = 1; i <= max_n; i++) {
         fib_numbers.push(fib(i))
     }
-    for (const [i, fib_number] of fib_numbers.entries()) {
+    
+    // Type the enumeration of the fib entries as a tuple of two numbers
+    let enumeration: [number, number]
+    for (enumeration of fib_numbers.entries()) {
         const current_output_element = document.createElement("div")
-        current_output_element.innerText = `${i}: ${fib_number}`
+        current_output_element.innerText = `${enumeration[0]}: ${enumeration[1]}`
         output_element.appendChild(current_output_element)
     }
 })
 
 const fib = (current_number: number, memowise: {[key: number]: number} = {}): number => {
-    if (current_number == 1) return 1
-    if (current_number == 2) return 1
+    // Check if we've already memoised the answer
     if (current_number in memowise) {
         return memowise[current_number]
+    }    
+    // The first two fib numbers are 1
+    if (current_number == 1) {
+        memowise[current_number] = 1
+        return 1
+    }
+    if (current_number == 2) {
+        memowise[current_number] = 1
+        return 1
     }
     
-    return fib(current_number - 1) + fib(current_number - 2)
+    const current_fib = fib(current_number - 1) + fib(current_number - 2)
+    memowise[current_number] = current_fib
+    return current_fib
 }
